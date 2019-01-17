baec3582ea5621fa6b276220be665bfd3b2a059c
Harlen Hobbs <hob14003@byui.edu>
2019-01-16 19:10:49 -0800

    testing

--- web/02Teach/02Teach.js
+++ web/02Teach/02Teach.js
@@ -3,5 +3,10 @@ function clicked() {
 }
 
 function colorChange() {
-    
+    var tb_id = "txtCol";
+    var tb = document.getElementById(tb_id);
+    var div_id = "t1";
+    var div = document.getElementById(div_id);
+    var color = textbox.value;
+    div.style.backgroundColor = color;
 }
