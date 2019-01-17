935d3e7f9ef13e747278057ae7e6d0cc93e8b835
Harlen Hobbs <hob14003@byui.edu>
2019-01-16 19:17:46 -0800

    trying to figure out color button

--- web/02Teach/02Teach.js
+++ web/02Teach/02Teach.js
@@ -3,10 +3,13 @@ function clicked() {
 }
 
 function colorChange() {
-    var tb_id = "txtCol";
-    var tb = document.getElementById(tb_id);
-    var div_id = "t1";
-    var div = document.getElementById(div_id);
-    var color = textbox.value;
-    div.style.backgroundColor = color;
+	var textbox_id = "txtColor";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "div1";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var color = textbox.value;
+	div.style.backgroundColor = color;
 }
