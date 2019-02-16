51842f4fde57f899004e6f440c7232854518a4f1
Harlen Hobbs <hob14003@byui.edu>
2019-02-16 11:57:44 -0800

    database interaction

--- web/05Prove/HandleSQL.php
+++ web/05Prove/HandleSQL.php
@@ -61,8 +61,8 @@ catch (PDOException $ex)
             $table = filter_var($_GET["table"]);
             $id = filter_var($_GET["id"]);
             echo "<p>table = " . $table . "<br>id = " . $id . "</p>"; 
-            echo "UPDATE " . $table . "SET NAME = '" . $name. "', DESCRIPTION = '" . $desc . "' WHERE ID = '" . $id. "';";
-            $db->query("UPDATE " . $table . "SET NAME = '" . $name. "', DESCRIPTION = '" . $desc . "' WHERE ID = '" . $id. "';");
+            echo "UPDATE " . $table . " SET NAME = '" . $name. "', DESCRIPTION = '" . $desc . "' WHERE ID = '" . $id. "';";
+            $db->query("UPDATE " . $table . " SET NAME = '" . $name. "', DESCRIPTION = '" . $desc . "' WHERE ID = '" . $id. "';");
         }
     }
 //header("Location: 3DomainsHome.php");
