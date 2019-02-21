8013ae721bf7ea4282b5afb27dcdbb90fe0d03a6
Harlen Hobbs <hob14003@byui.edu>
2019-02-20 16:00:02 -0800

    07Prove

---
+++ web/07Prove/3DomainsHome.php
@@ -0,0 +1,261 @@
+
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+  <title>3Domains Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>  
+    
+    <link rel="stylesheet" type="text/css" href="3Domain.css">
+    
+</head>
+<body>
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+<!-- LOGO -->
+    <div class="container-fluid no-padding bg-drkCell">
+        <img src="images/LOGO.jpg" class="img-fluid" alt="Responsive image">
+    </div>
+    
+<!-- CELL IMAGE -->
+<div class="container-fluid no-padding center">   
+    <div class="col-md-12">
+        <img src="images/blueCellsCrp.jpg" class="img-fluid crop height400" alt="Responsive image">
+  </div>
+</div>
+    
+    <!-- NAVBAR -->
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/02Prove/HomePage.php">CS 313 Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        
+      <li class="nav-item active">
+        <a class="nav-link" href="3DomainsHome.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+        
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Characteristics
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <?php
+                foreach ($db->query('SELECT DISTINCT c.NAME FROM DOMAIN d INNER JOIN CHARACTERISTIC c ON d.ID = c.DOMAIN_ID') as $row)
+                {
+                    echo "<a class=\"dropdown-item\" href=\"FilteredData.php?characteristic=" . $row[0] . "\">" . $row[0] . "</a>";
+                }
+                ?>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="FilteredData.php?characteristic=All Characteristics">All</a>
+        </div>
+      </li>
+        
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Components
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <?php
+                foreach ($db->query('SELECT DISTINCT c.NAME FROM DOMAIN d INNER JOIN COMPONENT c ON d.ID = c.DOMAIN_ID') as $row)
+                {
+                    echo "<a class=\"dropdown-item\" href=\"FilteredData.php?component=" . $row[0] . "\">" . $row[0] . "</a>";
+                }
+                ?>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="FilteredData.php?component=All Components">All</a>
+        </div>
+      </li>
+        
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Domains
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <?php
+                foreach ($db->query('SELECT DISTINCT NAME FROM DOMAIN') as $row)
+                {
+                    echo "<a class=\"dropdown-item\" href=\"FilteredData.php?domain=" . $row[0] . "\">" . $row[0] . "</a>";
+                }
+                ?>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="3DomainsHome.php">All</a>
+        </div>
+      </li>        
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Database
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="FilteredData.php?database=Insert">Insert</a>
+        </div>
+      </li>      
+    </ul>
+    <form action="FilteredData.php" method="get" class="form-inline my-2 my-lg-0">
+      <input name="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    
+    
+    
+    <!-- TABLES -->
+    
+<div class="container">
+  <div class="row">     
+    <div class="col-4">
+        
+        <h2>Bacteria</h2>
+      <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Characteristic</th>
+            <th scope="col">Description</th>
+            </tr>
+        </thead>
+        <tbody>      
+            <?php
+            foreach ($db->query('SELECT c.NAME, c.DESCRIPTION FROM DOMAIN d INNER JOIN CHARACTERISTIC c ON d.ID = c.DOMAIN_ID WHERE d.ID = 1;') as $row)
+            {
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "<td></tr>";
+            }
+            ?>           
+        </tbody>
+          <thead>
+            <tr>
+            <th scope="col">Component</th>
+            <th scope="col">Description</th>
+            </tr>
+        </thead>
+          <tbody>      
+            <?php
+            foreach ($db->query('SELECT c.NAME, c.DESCRIPTION FROM DOMAIN d INNER JOIN COMPONENT c ON d.ID = c.DOMAIN_ID WHERE d.ID = 1;') as $row)
+            {
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "<td></tr>";
+            }
+            ?>           
+        </tbody>
+      </table>
+    </div>     
+      <div class="col-4">  
+          
+       <h2>Archaea</h2>
+      <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Characteristic</th>
+            <th scope="col">Description</th>
+            </tr>
+        </thead>
+        <tbody>       
+            <?php
+            foreach ($db->query('SELECT c.NAME, c.DESCRIPTION FROM DOMAIN d INNER JOIN CHARACTERISTIC c ON d.ID = c.DOMAIN_ID WHERE d.ID = 2;') as $row)
+            {
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "<td></tr>";
+            }
+            ?>           
+        </tbody>
+        <thead>
+            <tr>
+            <th scope="col">Component</th>
+            <th scope="col">Description</th>
+            </tr>
+        </thead>
+          <tbody>      
+            <?php
+            foreach ($db->query('SELECT c.NAME, c.DESCRIPTION FROM DOMAIN d INNER JOIN COMPONENT c ON d.ID = c.DOMAIN_ID WHERE d.ID = 2;') as $row)
+            {
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "<td></tr>";
+            }
+            ?>           
+        </tbody>
+      </table>
+    </div>      
+      <div class="col-4">   
+        <h2>Eukarya</h2>
+          
+      <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Characteristic</th>
+            <th scope="col">Description</th>
+            </tr>
+        </thead>
+        <tbody>     
+            <?php
+            foreach ($db->query('SELECT c.NAME, c.DESCRIPTION FROM DOMAIN d INNER JOIN CHARACTERISTIC c ON d.ID = c.DOMAIN_ID WHERE d.ID = 3;') as $row)
+            {
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "<td></tr>";
+            }
+            ?>         
+        </tbody>
+        <thead>
+            <tr>
+            <th scope="col">Component</th>
+            <th scope="col">Description</th>
+            </tr>
+        </thead>
+          <tbody>      
+            <?php
+            foreach ($db->query('SELECT c.NAME, c.DESCRIPTION FROM DOMAIN d INNER JOIN COMPONENT c ON d.ID = c.DOMAIN_ID WHERE d.ID = 3;') as $row)
+            {
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "<td></tr>";
+            }
+            ?>           
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+    
+    <!-- CELL FOOTER -->
+<div class="container-fluid no-padding">
+  <div class="row">     
+    <div class="col-md-12">
+        <img src="images/blueCellsSprCrp.jpg" class="img-fluid crop" alt="Responsive image">
+    </div>
+      <div class="col-md-12">
+          <h1 class="bg-dark text-dark">.</h1>
+    </div>
+  </div>
+</div>
+    
+    
+</body>
+</html>
---
+++ web/07Prove/HandleSQL.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    </head>
+    <body>
+    
+<?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+    if($_GET["database"]) {
+
+            $dbCmd = filter_var($_GET["database"]);
+        //INSERT
+         if($dbCmd == "Insert"){
+             $charOrComp = filter_var($_POST["charOrComp"]);
+             $domains = $_POST["domains"];
+             $name = filter_var($_POST["name"]);
+             $desc = filter_var($_POST["desc"]);
+                echo "<p>charOrComp = " . $charOrComp . "<br>domains = " . $domains . "<br>name = " . $name . "<br>desc = " . $desc . "</p>";        
+             foreach($domains as $domain) {
+                 echo "INSERT INTO " . $charOrComp . "(DOMAIN_ID, NAME, DESCRIPTION) VALUES (" . $domain . ", " . $name . ", " . $desc . ");";
+             $db->query("INSERT INTO " . $charOrComp . "(DOMAIN_ID, NAME, DESCRIPTION) VALUES ('" . $domain . "', '" . $name . "', '" . $desc . "');");
+             }
+         }
+        //DELETE
+        if($dbCmd == "Delete"){
+            $table = filter_var($_GET["table"]);
+            $id = filter_var($_GET["id"]);
+            echo "<p>table = " . $table . "<br>id = " . $id . "</p>"; 
+            echo "DELETE FROM " . $table . " WHERE ID = '" . $id. "';";
+            $db->query("DELETE FROM " . $table . " WHERE ID = '" . $id. "';");
+        }
+        //EDIT
+        if($dbCmd == "Edit"){
+            $name = filter_var($_POST["name"]);
+            $desc = filter_var($_POST["desc"]);
+            $table = filter_var($_GET["table"]);
+            $id = filter_var($_GET["id"]);
+            echo "<p>table = " . $table . "<br>id = " . $id . "</p>"; 
+            echo "UPDATE " . $table . " SET NAME = '" . $name. "', DESCRIPTION = '" . $desc . "' WHERE ID = '" . $id. "';";
+            $db->query("UPDATE " . $table . " SET NAME = '" . $name. "', DESCRIPTION = '" . $desc . "' WHERE ID = '" . $id. "';");
+        }
+    }
+header("Location: 3DomainsHome.php");
+?>
+    </body>
+</html>
