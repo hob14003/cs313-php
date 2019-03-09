cd2e4fd7906435f1f6815af55692e1e8c10a1a5e
Harlen Hobbs <hob14003@byui.edu>
2019-03-09 08:50:05 -0800

    test

--- web/09Prove/postalRateCalc.js
+++ web/09Prove/postalRateCalc.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => res.send('Hello World!3'))
+
+app.get('/', function (req, res) {
+  res.send('Hello World!3')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
