feec1248161a7415100b10b5e76df9756585bed3
Harlen Hobbs <hob14003@byui.edu>
2019-03-15 16:55:58 -0700

    home?

--- web/10Prove/index.js
+++ web/10Prove/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -8,3 +8,25 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+*/
+
+var express = require('express');
+var app = express();
+var url = require('url');
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.get('/home', function(req, res) {
+    console.log("made it to home");
+});
